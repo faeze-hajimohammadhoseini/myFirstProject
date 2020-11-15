7343567e98dbc3a7cf7bd8d6c67d1a6763df02e9
setayesh hoseiini <setayesh.hoseiini79@gmail.com>
2020-11-15 20:21:36 +0330

    validation password (have bug to get password)

--- app/src/main/java/com/example/myapplication/MainActivity.java
+++ app/src/main/java/com/example/myapplication/MainActivity.java
@@ -1,5 +1,6 @@
 package com.example.myapplication;
 
+import android.annotation.SuppressLint;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
@@ -20,7 +21,12 @@ public class MainActivity extends AppCompatActivity {
     private Button btnLogin;
     private EditText edtUsername;
     private TextInputLayout edtPassword;
+    private TextView checkNumber;
+    private TextView checkUpperCase;
+    private TextView checkLowerCase;
 
+
+    @SuppressLint("WrongViewCast")
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -29,7 +35,16 @@ public class MainActivity extends AppCompatActivity {
 
         btnLogin = findViewById(R.id.btn_submit);
         edtUsername = findViewById(R.id.edit_username);
-        edtPassword =  (TextInputLayout) findViewById(R.id.edit_password);
+        edtPassword = (TextInputLayout) findViewById(R.id.edit_password);
+        checkNumber = findViewById(R.id.checkNumber);
+        checkLowerCase = findViewById(R.id.checkLowerCase);
+        checkUpperCase = findViewById(R.id.checkUpperCase);
+
+
+        checkNumber.setVisibility(View.GONE);
+        checkLowerCase.setVisibility(View.GONE);
+        checkUpperCase.setVisibility(View.GONE);
+
 
         btnLogin.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -39,18 +54,70 @@ public class MainActivity extends AppCompatActivity {
 
                 if (username.equals("") || password.equals("")) {
                     Toast.makeText(MainActivity.this, "Username or Password are empty", Toast.LENGTH_LONG).show();
-                        if (username.equals("setayesh")&& password.equals(false)){
-                            Toast.makeText(MainActivity.this, "password is wrong", Toast.LENGTH_LONG).show();
-                        }
+                    if (username.equals("setayesh") && password.equals(false)) {
+                        Toast.makeText(MainActivity.this, "password is wrong", Toast.LENGTH_LONG).show();
+                    }
+//                    if (username.equals("setayesh") && password.length()<8){
+//                        checkNumber.setVisibility(View.VISIBLE);
+//                    }
+
                 } else {
                     if (username.equals("setayesh") && password.equals("1234")) {
-                        Intent i = new Intent(MainActivity.this,AdvertisementActivity.class);
+                        Intent i = new Intent(MainActivity.this, AdvertisementActivity.class);
                         startActivity(i);
                         finish();
                     }
                 }
 
+
+                // validation a password
+
+                boolean hasUppercase = false;
+                boolean hasLowercase = false;
+
+
+                for (int i = 0; i < password.length(); i++) {
+
+                    if (Character.isUpperCase(password.charAt(i)))
+
+                        hasUppercase = true;
+
+                    else if (Character.isLowerCase(password.charAt(i)))
+                        hasLowercase = true;
+                }
+
+                if (username.equals("setayesh") && password.length() < 8) {
+                    checkNumber.setVisibility(View.VISIBLE);
+                }
+                if (username.equals("setayesh") && hasUppercase == false) {
+                    checkUpperCase.setVisibility(View.VISIBLE);
+                }
+                if (username.equals("setayesh") && hasLowercase == false) {
+                    checkLowerCase.setVisibility(View.VISIBLE);
+                }
+                if (username.equals("setayesh") && hasUppercase == false && hasLowercase == false) {
+                    checkLowerCase.setVisibility(View.VISIBLE);
+                    checkUpperCase.setVisibility(View.VISIBLE);
+                }
+                if (username.equals("setayesh") && hasUppercase == false && password.length() < 8) {
+                    checkUpperCase.setVisibility(View.VISIBLE);
+                    checkNumber.setVisibility(View.VISIBLE);
+                }
+                if (username.equals("setayesh") && hasLowercase == false && password.length() < 8) {
+                    checkUpperCase.setVisibility(View.VISIBLE);
+                    checkNumber.setVisibility(View.VISIBLE);
+                }
+                if (username.equals("setayesh") && hasLowercase == false && hasUppercase == false && password.length() < 8) {
+                    checkUpperCase.setVisibility(View.VISIBLE);
+                    checkNumber.setVisibility(View.VISIBLE);
+                    checkUpperCase.setVisibility(View.VISIBLE);
+                }
+
             }
+
         });
+
     }
+
+
 }
