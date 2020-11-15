85b34344654d93bb5c610df067d179468b76b4e3
setayesh hoseiini <setayesh.hoseiini79@gmail.com>
2020-11-15 18:35:09 +0330

    use icon to show password

--- app/src/main/java/com/example/myapplication/MainActivity.java
+++ app/src/main/java/com/example/myapplication/MainActivity.java
@@ -13,28 +13,29 @@ import android.os.Bundle;
 import android.widget.TextView;
 import android.widget.Toast;
 
+import com.google.android.material.textfield.TextInputEditText;
+import com.google.android.material.textfield.TextInputLayout;
+
 public class MainActivity extends AppCompatActivity {
     private Button btnLogin;
     private EditText edtUsername;
-    private EditText edtPassword;
+    private TextInputLayout edtPassword;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login_page);
 
-//        Intent i = new Intent(MainActivity.this,SplashActivity.class);
-
 
         btnLogin = findViewById(R.id.btn_submit);
         edtUsername = findViewById(R.id.edit_username);
-        edtPassword = findViewById(R.id.edit_password);
+        edtPassword =  (TextInputLayout) findViewById(R.id.edit_password);
 
         btnLogin.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                String username = edtUsername.getText().toString().toLowerCase().trim();
-                String password = edtPassword.getText().toString().toLowerCase().trim();
+                String username = edtUsername.getText().toString().trim();
+                String password = edtPassword.getEditText().toString().trim();
 
                 if (username.equals("") || password.equals("")) {
                     Toast.makeText(MainActivity.this, "Username or Password are empty", Toast.LENGTH_LONG).show();
