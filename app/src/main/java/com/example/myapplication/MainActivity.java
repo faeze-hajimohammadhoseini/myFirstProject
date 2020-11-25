41859a9407c7634e7dd73032ee6cd252c75e3c80
setayesh hoseiini <setayesh.hoseiini79@gmail.com>
2020-11-25 12:11:00 +0330

    fix error about click listener for text sign up in login activiry

--- app/src/main/java/com/example/myapplication/MainActivity.java
+++ app/src/main/java/com/example/myapplication/MainActivity.java
@@ -1,13 +1,10 @@
 package com.example.myapplication;
 
-import android.annotation.SuppressLint;
 import android.content.Intent;
 import android.os.Bundle;
-import android.telephony.ims.ImsReasonInfo;
 import android.view.View;
 import android.widget.Button;
 import android.widget.CheckBox;
-import android.widget.CompoundButton;
 import android.widget.EditText;
 import android.widget.TextView;
 
